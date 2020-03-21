85cd59e3ea268fd2c1762989e8532fa332d87880
Kunal Booch <kunalbooch@hotmail.com>
2020-03-21 11:58:56 +0530

    Feature Upload
    Features added: Copy functionality

--- lwr_nmbr.js
+++ lwr_nmbr.js
@@ -1,4 +1,16 @@
+var copyString;
 function generate(){
-var randomstring = Math.random().toString(36).slice(-12);
+randomstring = Math.random().toString(36).slice(-12);
+copyString = randomstring;
 document.getElementById('password_lower').innerHTML = randomstring ; //onClick
 }
+function executeCopy() {
+  var copyhelper = document.createElement("input");
+  copyhelper.className = 'copyhelper'
+  document.body.appendChild(copyhelper);
+  copyhelper.value = copyString;
+  copyhelper.select();
+  document.execCommand("copy");
+  document.body.removeChild(copyhelper);
+  alert("your results has been copied");
+};
